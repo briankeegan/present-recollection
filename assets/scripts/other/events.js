0b663fc6dd3c3b6f315860010c187fcc82c27c01
briankeegan <brian.morris.keegan@gmail.com>
2018-02-11 10:40:29 -0500

    Display content on DOM load
    - Change body to none in `display.scss`
    - Body display change to 'block' when page loads in `other\events.js`
    - This will prevent page from showing un-styled HTML

--- assets/scripts/other/events.js
+++ assets/scripts/other/events.js
@@ -8,6 +8,7 @@ const onLoadContent = () => ui.loadContent()
 const onRemoveContent = () => ui.removeContent()
 
 const addHandler = function () {
+  document.body.style.display = 'block'
   $('#open-about-modal').on('click', onOpenAboutModal)
   $('#present-recollection-modal').on('shown.bs.modal', onFocusInput)
   $('#present-recollection-modal').on('hidden.bs.modal', onRemoveContent)
